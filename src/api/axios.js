c3b68ded1d127b2d26f9249dcd69b64da1f8a437
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-22 10:17:02 +0100

    n

---
+++ src/api/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const api = axios.create ({
+    baseUrl: "https://jsonplaceholder.typicode.com/"
+})
+
+export const getPostsPage = async(pageParam = 1, Options = {}) => {
+    const response = await api.get(`/posts?_page=${pageParam}`, Options)
+    return response.data
+}
