2d35bb7b0e5f4ce7241fc18b2fae7b3394457fea
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-22 10:28:28 +0100

    a

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,2 +1,15 @@
 import { useState, useEffect } from "react";
 import { getPostsPage } from "../api/axios";
+
+
+const usePosts = () => {
+    const [ results, setResults ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState({})
+    const [ isError, setIsError ] = useState(false)
+    const [ hasNextPage, setHasNextPage ] = useState(false)
+
+  return { loading, isError, error, results, hasNextPage }
+}
+
+export default usePosts
