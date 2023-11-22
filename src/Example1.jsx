4c202b21c2e60d8885e29c51cb06d80346a977ae
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-22 11:38:27 +0100

    a

--- src/Example1.jsx
+++ src/Example1.jsx
@@ -3,7 +3,7 @@ import { useState, useRef, useCallback } from "react";
 import usePosts from "./hooks/usePosts";
 import { confirmAlert } from "react-confirm-alert";
 import 'react-confirm-alert/src/react-confirm-alert.css';
-import { isError } from "react-query"
+import { isError } from "react-query";
 import Post from "./Post"
 
 const Example1 = () => {
