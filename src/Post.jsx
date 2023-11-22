f6dda1834ced0c6579385f672857e75ed720808e
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-22 11:48:09 +0100

    m

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Post = React.forwardRef(() => {
-  return (
-    <div>Post</div>
-  )
+const Post = React.forwardRef(([post]) => {
+    const postBody = (
+        <>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p>Post ID: {post.title}</p>
+        </>
+    )
+
+    const content = ref ? (
+        <article ref={ref}>{postBody}</article>
+    ) : (
+        <article>{postBody}</article>
+    )
+
+  return content
 })
 
 export default Post
