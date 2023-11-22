2eae80e67a4b786677ade6939355136afdb74ca4
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-22 10:04:47 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Example1 from './Example1';
 
 function App() {
   return (
     <div className="App">
-      
+      <Example1/>
     </div>
   );
 }
